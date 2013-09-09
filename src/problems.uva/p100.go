2f7d89632865e61656b851576eb38d1193b5bca8
EV <uibuddha@gmail.com>
2013-09-09 17:35:38 +0800

    Code cleanup

--- src/problems.uva/p100.go
+++ src/problems.uva/p100.go
@@ -4,66 +4,48 @@ import (
   "fmt"
   "os"
 	"strconv"
-	"time"
+	"util"
 )
 
 func Run() {
-	startTime := time.Now() 
 	args := os.Args[1:]
 	for i, j, n:= 0, 1, len(args); i < n && j < n; {
 		num1, err := strconv.Atoi(args[i])
 		if(err == nil) {
 	    num2, err := strconv.Atoi(args[j])
 	    if(err == nil) {
-				_100(num1, num2)
-				i = i+2
-				j = j+2
+				printMaxCycle(num1, num2)
+				i += 2
+				j += 2
 			} else {
-				fmt.Println(err)
-				os.Exit(2) 
+				common.PrintErrorAndExit(err)
 	    }
 		} else {
-			fmt.Println(err)
-	    os.Exit(2)
+			common.PrintErrorAndExit(err)
 		}
 	}
-	elapsedTime := (time.Now().Sub(startTime))
-	fmt.Println("Elapsed time: ", elapsedTime)
 }
 
-type _100Max struct {
-	allNum []int
-	cycle int
-	num int
-}
-
-func _100(i, j int) {
-	cycle, cnt:= 0, 0
-	obj := _100Max{[]int{} , 0, 0}
-	for n := i ; n <= j; n++ {
-		cnt = 0
-		cycle = _100Logic(n, &cnt)
-		if(obj.cycle == cycle) {
-	    obj.allNum = append(obj.allNum, n)
-		}
-		if(obj.cycle < cycle) {
-	    obj.cycle = cycle
-	    obj.allNum = []int {}
-	    obj.allNum = append(obj.allNum, n)
-	    obj.num = n
+func printMaxCycle(num1, num2 int) {
+	maxCycle, cycle:= 0, 0
+	for n := num1 ; n <= num2; n++ {
+		cycle = 0
+		loop(n, &cycle)
+		if(maxCycle < cycle) {
+	    maxCycle = cycle
 		}
 	}
-	fmt.Println(i, " ", j, " ",obj.cycle)
+	fmt.Println(num1, " ", num2, " ",maxCycle)
 }
 
-func _100Logic(n int, cnt *int) int {
-	*cnt++
+func loop(n int, cycle *int) int {
+	*cycle++
 	if (n != 1) {
 		if (n%2 == 1) {
-	    _100Logic(3*n+1, cnt)
+	    loop(3*n+1, cycle)
 		} else {
-	    _100Logic(n/2, cnt)
+	    loop(n/2, cycle)
 		}
 	}
-	return *cnt
+	return *cycle
 }
